2aa6364ac549415d211f3869a9aa702d02d314e6
Osi demo <admin@servesolutions.info>
2025-05-07 09:39:48 -0700

    error

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -81,7 +81,7 @@ resource "aws_route_table" "public_b" {
   }
 }
 
-resource "aws_route_table_association" "public_internet_access_b" {
+resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public_b.id
 
