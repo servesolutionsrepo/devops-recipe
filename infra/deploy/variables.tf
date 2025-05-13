d234ec2ba2ce63e86f421b2eb8c831c89335f6de
Osi demo <admin@servesolutions.info>
2025-05-13 09:29:20 -0700

    adding database

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -15,3 +15,16 @@ variable "contact" {
   type        = string
   default     = "nzeako_o@hotmail.com"
 }
+
+variable "db_username" {
+  description = "username for the recipe app api database"
+  type        = string
+  default     = "recipeapp"
+
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+  type        = string
+
+}
