a177047bf9c2f8282a074197bb12f8af693e6c58
Osi demo <admin@servesolutions.info>
2025-05-13 09:42:16 -0700

    policy change

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -177,7 +177,9 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:AddTagsToResource"
+
     ]
     resources = ["*"]
   }
